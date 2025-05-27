1bc3f3719c692de92ad1c92e6b8e3a2294a891cf
Fuyuharu12345 <carlcydrixdelapiedra@gmail.com>
2025-05-27 08:51:02 +0800

    imback

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CollegeStudent"},{"p":"<Unnamed>","l":"GradeLevel"},{"p":"<Unnamed>","l":"HighSchoolStudent"},{"p":"<Unnamed>","l":"QRCodeGenerator"},{"p":"<Unnamed>","l":"Student"},{"p":"<Unnamed>","l":"StudentManager"},{"p":"<Unnamed>","l":"StudentQRCodeApp"}];updateSearchResults();
